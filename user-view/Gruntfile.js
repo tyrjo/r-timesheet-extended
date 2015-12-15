abede226813139a3a053a78d794994570b8715cd
John Martin <curmudgeon+git@rallydev.com>
2015-12-15 14:55:27 -0800

    reorganize

--- user-view/Gruntfile.js
+++ user-view/Gruntfile.js
@@ -10,7 +10,12 @@ module.exports = function(grunt) {
     
         config = grunt.file.readJSON('config.json');
 
-        config.js_files = grunt.file.expand(['src/javascript/utils/*.js','src/javascript/*.js']);
+        config.js_files = grunt.file.expand([
+            '../common/src/javascript/utils/*.js',
+            '../common/src/javascript/*.js',
+            'src/javascript/utils/*.js',
+            'src/javascript/*.js'
+        ]);
 
         config.ugly_files = grunt.file.expand(['deploy/app.min.*.js']);
         
