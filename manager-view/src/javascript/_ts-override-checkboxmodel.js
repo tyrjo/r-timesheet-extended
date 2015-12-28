7ffe60c004168fcf40babd5d7b07827786548b97
John Martin <curmudgeon+git@rallydev.com>
2015-12-27 20:27:34 -0800

    expand all on click checkbox

---
+++ manager-view/src/javascript/_ts-override-checkboxmodel.js
@@ -0,0 +1,23 @@
+Ext.override(Ext.selection.CheckboxModel,{
+    onHeaderClick: function(headerCt, header, e) {
+        // force open all if checking.
+        var view = this.view;
+        console.log('view:', view);
+        view.features[0].expandAll();
+        
+        if (header.isCheckerHd) {
+            e.stopEvent();
+            var me = this,
+                isChecked = header.el.hasCls(Ext.baseCSSPrefix + 'grid-hd-checker-on');
+                
+            // Prevent focus changes on the view, since we're selecting/deselecting all records
+            me.preventFocus = true;
+            if (isChecked) {
+                me.deselectAll();
+            } else {
+                me.selectAll();
+            }
+            delete me.preventFocus;
+        }
+    }
+});
